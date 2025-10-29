7fa2834b73b3b890426e1e9d44a2a8ab63402652
Piotrek Szyszko <piotrszyszko86@gmail.com>
2025-10-29 09:12:57 +0100

    Initial project setup for Rouglike
    Add solution, project files, and .gitignore for the Rouglike application. Includes a basic Program.cs with a Hello World message and initial configuration for .NET 9.0.

---
+++ Rouglike/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Rouglike;
+
+internal static class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Naciśnij ENTER żeby zakończyć...");
+    }
+}
