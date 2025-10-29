853f9703b849de1e3ce657f4e22f0c136346fb6d
Piotrek Szyszko <piotrszyszko86@gmail.com>
2025-10-29 11:47:03 +0100

    Add user input prompts for name and location
    The program now asks the user for their name and location, then greets them using the provided information. An additional Console.ReadLine() was added to pause before exiting.

--- Rouglike/Program.cs
+++ Rouglike/Program.cs
@@ -4,7 +4,14 @@ internal static class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Jak się nazywasz Nieznajomy?");
+        var imię = Console.ReadLine();
+        
+        Console.WriteLine($"Skąd jesteś {imię}?");
+        var miejsce = Console.ReadLine();
+        Console.WriteLine($"Witaj {imię} z {miejsce}!");
+        
         Console.WriteLine("Naciśnij ENTER żeby zakończyć...");
+        Console.ReadLine();
     }
 }
